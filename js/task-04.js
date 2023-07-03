7229f2ddd5b3ecea482f812bf4524fc4eb69ccd6
Dmytro Chumak <dchumakwork@gmail.com>
2023-07-03 11:22:03 +0300

    Complete
    ready

--- js/task-04.js
+++ js/task-04.js
@@ -6,23 +6,24 @@ function getRandomHexColor() {
 const boxesRef = document.getElementById('boxes');
 const createBtnRef = document.querySelector('button[data-create]');
 const destrBtnRef = document.querySelector('button[data-destroy]');
+const inputRef = document.querySelector('input');
 
 createBtnRef.addEventListener('click', createBoxes);
 
 function createBoxes(amount) {
   let divArr = [];
-  let diveSize = {height: 30, weight: 30}
-  for (let i = 1; i <= amount; i++) {
+  let divSize = {height: 30, width: 30}
+  for (let i = 1; i <= inputRef.value; i++) {
     const div = document.createElement('div');
-    div.style.height=`${diveSize.height} px`;
-    div.style.weight=`${diveSize.weight} px`;
-    div.style.color=getRandomHexColor;
-    diveSize.height+=10;
-    diveSize.weight+=10;
+    div.style.height=`${divSize.height}px`;
+    div.style.width=`${divSize.width}px`;
+    div.style.backgroundColor=getRandomHexColor();
+    divSize.height+=10;
+    divSize.width+=10;
     divArr.push(div);
   }
-  const markDiv = divArr.join("");
-  boxesRef.insertAdjacentHTML("afterbegin", markDiv);
-
+  // const markDiv = divArr.join(" ");
+  // boxesRef.insertAdjacentHTML("afterbegin", markDiv);
+  boxesRef.prepend(...divArr);
   return;
 }

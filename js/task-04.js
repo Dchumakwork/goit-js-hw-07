527bb0292bd5ede09f7712fbabc0d667777923d7
Dmytro Chumak <dchumakwork@gmail.com>
2023-07-03 11:39:51 +0300

    +

--- js/task-04.js
+++ js/task-04.js
@@ -3,27 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const boxesRef = document.getElementById('boxes');
-const createBtnRef = document.querySelector('button[data-create]');
-const destrBtnRef = document.querySelector('button[data-destroy]');
-const inputRef = document.querySelector('input');
+const boxesRef = document.getElementById("boxes");
+const createBtnRef = document.querySelector("button[data-create]");
+const destrBtnRef = document.querySelector("button[data-destroy]");
+const inputRef = document.querySelector("input");
 
-createBtnRef.addEventListener('click', createBoxes);
+createBtnRef.addEventListener("click", createBoxes);
 
 function createBoxes(amount) {
   let divArr = [];
-  let divSize = {height: 30, width: 30}
+  let divSize = { height: 30, width: 30 };
   for (let i = 1; i <= inputRef.value; i++) {
-    const div = document.createElement('div');
-    div.style.height=`${divSize.height}px`;
-    div.style.width=`${divSize.width}px`;
-    div.style.backgroundColor=getRandomHexColor();
-    divSize.height+=10;
-    divSize.width+=10;
+    const div = document.createElement("div");
+    div.style.height = `${divSize.height}px`;
+    div.style.width = `${divSize.width}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    divSize.height += 10;
+    divSize.width += 10;
     divArr.push(div);
   }
   // const markDiv = divArr.join(" ");
   // boxesRef.insertAdjacentHTML("afterbegin", markDiv);
   boxesRef.prepend(...divArr);
   return;
+}
+
+destrBtnRef.addEventListener("click", destroyBoxes);
+
+function destroyBoxes () {
+  boxesRef.innerHTML = "";
 }
